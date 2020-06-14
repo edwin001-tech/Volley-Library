c739b963fa5a7b41605de8fc6a2c2c4626d56b2a
edwin001-tech <edwinshitakwa8@gmail.com>
2020-06-14 11:59:25 +0300

    parsing JsonArray

--- app/src/main/java/com/example/parsing/MainActivity.java
+++ app/src/main/java/com/example/parsing/MainActivity.java
@@ -9,9 +9,12 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,7 +35,11 @@ public class MainActivity extends AppCompatActivity {
         new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                Log.d("JSON:", "onResponse" +response);
+                try {
+                    Log.d("JSON:", "onResponse: " +response.getString("title"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
             }
         }, new Response.ErrorListener() {
@@ -42,6 +49,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        requestQueue.add(jsonObjectRequest);
+
+        //JsonArray Request
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET,
+                "https://jsonplaceholder.typicode.com/todos", null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d("JsonArray", "onResponse" +response);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        //requestQueue.add(jsonObjectRequest);
+        requestQueue.add(jsonArrayRequest);
     }
 }
